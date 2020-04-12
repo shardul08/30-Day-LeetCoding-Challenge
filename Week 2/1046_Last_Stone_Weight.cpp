389aaf8b60846f9527e10928c1aafa1ee0b27085
Shardul Silswal <sshardul007@gmail.com>
2020-04-12 18:02:34 +0530

    12 Apr

---
+++ Week 2/1046_Last_Stone_Weight.cpp
@@ -0,0 +1,74 @@
+/*
+We have a collection of stones, each stone has a positive integer weight.
+
+Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+    If x == y, both stones are totally destroyed;
+    If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+ 
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+
+ 
+
+Note:
+
+    1 <= stones.length <= 30
+    1 <= stones[i] <= 1000
+
+*/
+
+//Solution 1
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        if(stones.size() == 1)
+            return stones[0];
+        sort(stones.begin(),stones.end(),greater<int>());
+
+        while(stones[1] != 0) {
+            stones[0] = stones[0] - stones[1];
+            stones[1] = 0;
+            sort(stones.begin(),stones.end(),greater<int>());
+        }
+        return stones[0];        
+    }
+};
+
+//Solution 2 Using Priority Queue
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(),stones.end());
+        while(pq.size() > 1) {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            if(x-y)
+                pq.push(x-y);
+        }
+        if(pq.size())
+            return pq.top();
+        return 0;
+    }
+};
+
+/*
+Complexity analysis
+Time: O(NlogN)
+Space: O(N)
+*/
