52b31550d756661a484120376261008259701012
Shardul Silswal <sshardul007@gmail.com>
2020-04-30 13:44:27 +0530

    30 Apr

---
+++ Week 5/Check_If_a_String_Is_a_Valid_Sequence_from_Root_to_Leaves_Path_in_a_Binary_Tree.cpp
@@ -0,0 +1,64 @@
+/*
+Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree. 
+
+We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
+
+ 
+
+Example 1:
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,0,1]
+Output: true
+Explanation: 
+
+The path 0 -> 1 -> 0 -> 1 is a valid sequence (green color in the figure). 
+Other valid sequences are: 
+0 -> 1 -> 1 -> 0 
+0 -> 0 -> 0
+
+Example 2:
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,0,1]
+Output: false 
+Explanation: The path 0 -> 0 -> 1 does not exist, therefore it is not even a sequence.
+
+Example 3:
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,1]
+Output: false
+Explanation: The path 0 -> 1 -> 1 is a sequence, but it is not a valid sequence.
+
+ 
+
+Constraints:
+
+    1 <= arr.length <= 5000
+    0 <= arr[i] <= 9
+    Each node's value is between [0 - 9].
+
+*/
+
+//Solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidSequence(TreeNode* root, vector<int>& arr, int pos=0) {
+        if(root == NULL || pos >= arr.size() || root->val != arr[pos])
+            return false;
+
+        if(pos == arr.size()-1 && root->left == NULL && root->right == NULL)
+            return true;
+        return isValidSequence(root->left,arr,pos+1) || isValidSequence(root->right,arr,pos+1);        
+    }
+};
